462644a202a176b6576920e78237fdb5f98c4191
Arun Appoos <54317137+Cosmic-Bug@users.noreply.github.com>
2021-12-24 20:04:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -41,8 +41,8 @@ module.exports = {
     DEBUG: DEBUG,
     REMOVEBG: process.env.REMOVEBG_KEY === undefined ? "false" : process.env.REMOVEBG_KEY,
     WARN_COUNT: process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
-    WARN_MSG: process.env.WARN_MSG === undefined ? "Ok bie" : process.env.WARN_MSG,
+    WARN_MSG: process.env.WARN_MSG === undefined ? "Vilachil edukkaruth kettoo" : process.env.WARN_MSG,
     ANTIJID: process.env.ANTIJID === undefined ? '' : process.env.ANTIJID,
-    STICKER_PACKNAME: process.env.STICKER_PACKNAME === undefined ? '🥰,lyfe00011' : process.env.STICKER_PACKNAME,
+    STICKER_PACKNAME: process.env.STICKER_PACKNAME === undefined ? 'Buggy👾' : process.env.STICKER_PACKNAME,
     BRAINSHOP: process.env.BRAINSHOP === undefined ? "159501,6pq8dPiYt7PdqHz3" : process.env.BRAINSHOP
 };
